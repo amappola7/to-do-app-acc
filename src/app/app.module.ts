07866fa1591adddcb7bbee309f2e3dd3688e422e
Amappola <anam.porrasp@gmail.com>
2024-04-12 11:19:30 -0500

    Creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './ui/shared/header/header.component';
+import { NavBarComponent } from './ui/shared/nav-bar/nav-bar.component';
+import { TaskComponent } from './ui/shared/task/task.component';
+import { SectionTitleComponent } from './ui/shared/section-title/section-title.component';
+import { ToDoListComponent } from './ui/pages/to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    TaskComponent,
+    SectionTitleComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule,
